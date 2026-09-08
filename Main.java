fa9178d419d0d21e3e9b258fdfdd2eef170990c0
Altyn-se2513 <250886@astanait.edu.kz>
2026-09-08 17:11:47 +0500

    Refactor Main class to use CarDirector for construction

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        // Сборка спортивного авто через цепочку методов (Method Chaining)
-        Car sportsCar = new Car.Builder()
-                .setSeats(2)
-                .setEngine("V8 Turbo")
-                .enableGps()
+        CarDirector director = new CarDirector();
+
+        Car.Builder sportsBuilder = new Car.Builder();
+        director.constructSportsCar(sportsBuilder);
+        Car sportsCar = sportsBuilder.build();
+
+        Car.Builder cityBuilder = new Car.Builder();
+        director.constructCityCar(cityBuilder);
+        Car cityCar = cityBuilder.build();
+
+        Car customCar = new Car.Builder()
+                .setSeats(7)
+                .setEngine("V6 Diesel")
                 .enableTripComputer()
                 .build();
 
-        // Сборка базового авто
-        Car baseCar = new Car.Builder()
-                .setSeats(5)
-                .build();
-
-        System.out.println("Спортивная машина: " + sportsCar);
-        System.out.println("Базовая машина: " + baseCar);
+        System.out.println("Sports Car: " + sportsCar);
+        System.out.println("City Car:   " + cityCar);
+        System.out.println("Custom Car: " + customCar);
     }
 }
